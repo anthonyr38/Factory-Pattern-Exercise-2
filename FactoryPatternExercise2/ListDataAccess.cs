d7019997af2f539832e79d123cef46f51f2bd5a7
Anthony Rodriguez <anthonyr38@proton.me>
2023-05-31 09:28:28 -0400

    Factory Pattern Exercise 2

---
+++ FactoryPatternExercise2/ListDataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class ListDataAccess : IDataAccess
+    {
+        public List<Product> LoadData()
+        {
+
+            Console.WriteLine("Loading from a list");
+            return new List<Product>();
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("Saving Data to a list db.");
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What db woud you like to use? ('SQL', 'Mongo', or 'list'?");
+            var dbType = Console.ReadLine();
+
+            var dataAccess = DataAccessFactory.GetDatabase(dbType);
+
+            List<Product> products = dataAccess.LoadData();
+            dataAccess.SaveData();
         }
     }
 }
